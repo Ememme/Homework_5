3ec340bf758e4b78d04180d65cc9bd3c36499793
Magda Mieczynska <magda.mieczynska@gmail.com>
2019-04-30 20:49:13 +0200

    Refactor if to switch

--- fibonacci.js
+++ fibonacci.js
@@ -1,23 +1,23 @@
 function fibonacci(number) {
-
-    if (number < 0 ) {
-        throw new Error(`Please provide a valid argument. Fibonacci sequence is valid only for numbers >= 0`);
-
+    if (number < 0) {
+        return (`Please provide a valid argument. Fibonacci is valid only for numbers >= 0`);
     }
 
     if (!Number.isInteger(number)) {
         return (`Please provide a valid integer input`);
     }
 
-    if (number === 0) return [];
-    if (number === 1) return [0];
-    if (number === 2) return [0,1];
+    switch(number) {
+        case 0: return [];
+        case 1: return [0];
+        case 2: return [0, 1];
+    }
 
     let fibArray = [0, 1];
     for (let i = 2; i < number; i++) {
         fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
     }
-        
+
     return fibArray;
 }
 
