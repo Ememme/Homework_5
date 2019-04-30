71c162fb8329dad8fbe2cb9e6fe6d0f095d14ff5
Magda Mieczynska <magda.mieczynska@gmail.com>
2019-04-30 20:49:41 +0200

    Improve indentation

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -7,7 +7,7 @@ const fibo = require('./fibonacci');
 
 // Test boundary conditions
 test('finds fibo for 0', () => {
-  expect(fibo(0)).toEqual([]);
+    expect(fibo(0)).toEqual([]);
 });
 
 test('finds fibo for 1', () => {
@@ -29,29 +29,28 @@ test('finds fibo for 4', () => {
 
 // Validates input format 
 // Case: Negative number
-test ('checks if input is an integer', ()=> {
-    expect(fibo(-1)).toEqual('Please provide a valid argument. Fibonacci sequence is valid only for numbers >= 0');
+test('checks if input is an integer', () => {
+    expect(fibo(-1)).toEqual('Please provide a valid argument. Fibonacci is valid only for numbers >= 0');
 })
 
 // Case: Float
-test ('checks if input is an integer', ()=> {
+test('checks if input is an integer', () => {
     expect(fibo(0.3333)).toEqual("Please provide a valid integer input");
 })
 // Case: String
-test ('checks if input is an integer', ()=> {
+test('checks if input is an integer', () => {
     expect(fibo('DaftAcademy')).toEqual("Please provide a valid integer input");
 })
 // Case: Object
-test ('checks if input is an integer', ()=> {
+test('checks if input is an integer', () => {
     expect(fibo({})).toEqual("Please provide a valid integer input");
 })
 
 // Validates output 
-test ('checks if output is an array', ()=> {
+test('checks if output is an array', () => {
     expect(fibo(1)).toBeInstanceOf(Array);
 })
 
-test ('checks if output array is of correct length', ()=> {
+test('checks if output array is of correct length', () => {
     expect(fibo(8)).toHaveLength(8);
 })
-
