cccaac47ea5c4e5a302b93eff87df5c4a2cd6157
Magda Mieczynska <magda.mieczynska@gmail.com>
2019-04-30 20:11:08 +0200

    Refactor: add conditions for input checking

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,14 @@
 function fibonacci(number) {
+
+    if (number < 0 ) {
+        throw new Error(`Please provide a valid argument. Fibonacci sequence is valid only for numbers >= 0`);
+
+    }
+
+    if (!Number.isInteger(number)) {
+        return (`Please provide a valid integer input`);
+    }
+
     if (number === 0) return [];
     if (number === 1) return [0];
     if (number === 2) return [0,1];
